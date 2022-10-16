f11ea7d6f098d9cff6140231aa019c8749b97bd6
YutoKatsuno <104402349+YutoKatsuno@users.noreply.github.com>
2022-10-16 18:35:00 +0900

    Adjust so as not to violate the terms of use

--- insta_follower.py
+++ insta_follower.py
@@ -3,6 +3,7 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import ElementClickInterceptedException
 
 
 class InstaFollower:
@@ -21,8 +22,12 @@ class InstaFollower:
         input_password = self.driver.find_element(By.NAME, "password")
         input_password.send_keys(password, Keys.ENTER)
 
-    def find_followers(self):
-        pass
+    def get_followers(self, account):
+        self.driver.get(f"https://www.instagram.com/{account}")
+
+        time.sleep(3)
+        followers_button = self.driver.find_element(By.XPATH, '')
+        followers_button.click()
 
     def follow(self):
         pass
--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import os
 from insta_follower import InstaFollower
 
-SIMILAR_ACCOUNT = "chefsteps"
+SIMILAR_ACCOUNT = ""
 USERNAME = os.environ["USERNAME"]
 PASSWORD = os.environ["PASSWORD"]
 
 bot = InstaFollower()
 bot.login(USERNAME, PASSWORD)
-bot.find_followers()
+bot.get_followers(SIMILAR_ACCOUNT)
 bot.follow()
